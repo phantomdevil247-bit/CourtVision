17ee2678017e097ae7d5b9f3771ff481d1593b8d
Phantom Devil <PhantomDevil247@gmail.com>
2026-05-14 10:38:33 +1000

    added API key and headers to api_handler.py

--- api_handler.py
+++ api_handler.py
@@ -1,10 +1,18 @@
 import requests
 
+# Base URL and API key
 BASE_URL = "https://api.balldontlie.io/v1/"
+API_KEY = "b9f3d1bd-aca9-4b50-97f3-aa45ec432d52"
+
+# Headers to send with every request
+HEADERS = {
+    "Authorization": "b9f3d1bd-aca9-4b50-97f3-aa45ec432d52"
+}
+
 
 def get_players():
     try:
-        response = requests.get(f"{BASE_URL}/players?per_page=25")
+        response = requests.get(f"{BASE_URL}/players?per_page=25, headers=HEADERS")
         data = response.json()
         return data['data']
     except Exception as e:
@@ -13,7 +21,7 @@ def get_players():
     
 def get_standings():
     try:
-        response = requests.get(f"{BASE_URL}/standings?season=2025")
+        response = requests.get(f"{BASE_URL}/standings?season=2025,headers=HEADERS")
         data = response.json()
         return data['data']
     except Exception as e:
