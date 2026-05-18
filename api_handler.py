ead1e93c8e10fe6d5ed06355a23ef2913c652409
phantomdevil247-bit <PhantomDevil247@gmail.com>
2026-05-19 08:55:55 +1000

    fixed API call syntax - players now loading correctly

--- api_handler.py
+++ api_handler.py
@@ -2,17 +2,16 @@ import requests
 
 # Base URL and API key
 BASE_URL = "https://api.balldontlie.io/v1/"
-API_KEY = "b9f3d1bd-aca9-4b50-97f3-aa45ec432d52"
+API_KEY = "1de2bd8e-123a-4cfc-afe8-3e5aaed799de"
 
 # Headers to send with every request
 HEADERS = {
-    "Authorization": "b9f3d1bd-aca9-4b50-97f3-aa45ec432d52"
+    "Authorization": API_KEY
 }
-
 
 def get_players():
     try:
-        response = requests.get(f"{BASE_URL}/players?per_page=25, headers=HEADERS")
+        response = requests.get(f"{BASE_URL}/players?per_page=25", headers=HEADERS)
         data = response.json()
         return data['data']
     except Exception as e:
@@ -21,7 +20,7 @@ def get_players():
     
 def get_standings():
     try:
-        response = requests.get(f"{BASE_URL}/standings?season=2025,headers=HEADERS")
+        response = requests.get(f"{BASE_URL}/standings?season=2025",headers=HEADERS)
         data = response.json()
         return data['data']
     except Exception as e:
